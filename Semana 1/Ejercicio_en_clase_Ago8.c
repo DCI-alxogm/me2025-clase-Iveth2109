dcff42419fd1f15c407fa25481ec7bb7092b497c
Iveth2109 <02iveth21martinez@gmail.com>
2025-08-08 13:55:35 -0400

    Add files via upload

---
+++ Semana 1/Ejercicio_en_clase_Ago8.c
@@ -0,0 +1,18 @@
+//Ejercicio en clase 8 de agosto de 2025
+//Laura Iveth Martinez Prado
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float numeros[4];
+  numeros[0]=3.1;
+  numeros[1]=3.4;
+  numeros[2]=3.8;
+  numeros[3]=4.2;
+  numeros[4]=4.6;
+
+  printf("%f %f %f %f %f", numeros[0], numeros[1], numeros[2], numeros[3], numeros[4]);
+  printf("\nvalores de las funciones");
+  printf("\nfuncion x**2=%f", numeros[0]*numeros[0]);
+}
