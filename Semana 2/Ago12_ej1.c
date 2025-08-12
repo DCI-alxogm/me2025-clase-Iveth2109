d3f653366d3e6bcd324511bbdda48de20087bef3
Iveth2109 <02iveth21martinez@gmail.com>
2025-08-12 13:48:39 -0400

    Add files via upload

---
+++ Semana 2/Ago12_ej1.c
@@ -0,0 +1,36 @@
+//Agosto 12 ejercicio en clase
+//Laura Iveth Martinez Prado
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int numeros[5];
+  float resultados[5];
+  int indx;
+  float pi=3.1415926536;
+
+  numeros[0]=1;
+  numeros[1]=2;
+  numeros[2]=3;
+  numeros[3]=4;
+  numeros[4]=5;
+
+  //printf(" x\t x^2\t log(x+1)\t exp(x)+cos(x)");
+
+  for (indx=0; indx<5; indx++){
+    //printf("\n%d %d\n", numeros[indx], numeros[indx]%2);
+    if(numeros[indx]%2==0){
+      resultados[indx]=log10(numeros[indx]+1);
+      printf("x=%d \n log(x+1)= %f\n", numeros[indx], resultados[indx]); 
+  } else{
+    resultados[indx]=exp(numeros[indx])+cos(numeros[indx]);
+    printf(" x=%d \n exp(x)+cos(x)= %f\n", numeros[indx], resultados [indx]);
+  }
+}
+printf("resultados\n");
+for(indx=0; indx<5; indx++){
+  printf("%f\n", resultados[indx]);
+  }
+  printf("Pi+Pi=%.16lf", pi+pi);
+}
