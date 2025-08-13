c3466394fd663fc3be32460803bc876223a75dae
Iveth2109 <02iveth21martinez@gmail.com>
2025-08-13 18:03:31 -0400

    Add files via upload

---
+++ Semana 2/operaciones_float_Ago13_2.c
@@ -0,0 +1,21 @@
+//Laura Iveth Martinez Prado
+//13 de agosto 2025
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float a, b, c, d;
+  a=3;
+  b=4;
+  c=5;
+  d=7;
+
+  printf("Valores asignados");
+  printf("\n%f %f %f %f", a, b, c, d);
+  printf("\nValores de las funciones");
+  printf("\ne=(a+b)*c7d= %f", (a+b)*c/d);
+  printf("\ne=((a+b)*c)/d =%f", ((a+b)*c)/d);
+  printf("\ne=a+b*c/d= %f", a+b*c/d);
+  printf("\ne=a+(b*c)/d= %f", a+(b*c)/d);
+}
