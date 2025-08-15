cf65589df7748e8f83e26739facb69f20eefc0cd
Iveth2109 <02iveth21martinez@gmail.com>
2025-08-15 11:53:32 -0600

    Add files via upload

---
+++ Semana 2/Raíces_de_un_binomio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float a,b,c;
+  printf("Ingresa el valor de a\n");
+  scanf("%f", &a);
+  printf("\nIngresa el valor de b\n");
+  scanf("%f", &b);
+  printf("\nIngresa el valor de c\n");
+  scanf("%f", &c);
+
+  if(a==0);
+    if(b==0){
+    printf("No hay solución");
+    }
+    else{
+      x1=-c/b
+    }
+  else{
+    
+  }
+}
