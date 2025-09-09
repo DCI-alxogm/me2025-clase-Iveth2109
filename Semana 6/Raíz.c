3350921294a5b01819b029301a808921ce8bb6cb
Iveth2109 <02iveth21martinez@gmail.com>
2025-09-09 11:47:32 -0600

    Add files via upload

---
+++ Semana 6/Raíz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float c);
+
+int main(){
+  float a, b, k, Err_max, Err, Kold;
+  float k=(a+b)/2;
+
+  printf("Ingresa el valor de a:");
+  scanf("%f", &a);
+  printf("Ingresa el valor de b:");
+  scanf("%f", &b);
+
+while(f(k) f(a)<0){
+  if (f(k)f(a)<0){
+    b=k
+  } else{
+    a=k
+  }
+  k=kold;
+  }
+  Err=(k-kold)/k
+}
+
+float f(float c){
+  return (9.81*68.1)/c*(1-exp(-(c/68.1)*10))-40;
+}
