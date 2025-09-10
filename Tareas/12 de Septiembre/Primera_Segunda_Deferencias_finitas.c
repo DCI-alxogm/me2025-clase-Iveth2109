91e3e90edb58e10bbc489e7c6ced822b83505749
Iveth2109 <02iveth21martinez@gmail.com>
2025-09-10 14:12:09 -0600

    Add files via upload

---
+++ Tareas/12 de Septiembre/Primera_Segunda_Deferencias_finitas.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+
+float fx(float x);
+
+int main(){
+  int i;
+  float a=-2, b=2, h=0.25;
+  int n=(int)((b-a)/h)+1;
+  float x[n], fpa[n], fpb[n], fpc[n], sda[n], sdb[n], sdc[n];
+
+  for(i=0; i<n; i++){
+    x[i]=a+i*h;
+  }
+
+  for(i=0; i<n; i++){
+    fpa[i]=(fx(x[i+1])-fx(x[i]))/h;
+    fpb[i]=(fx(x[i])-fx(x[i-1]))/h;
+    fpc[i]=(fx(x[i+1])-fx(x[i-1]))/(2*h);
+  }
+
+  printf("Primera derivada\n");
+  printf("%-10s %-12s %-12s %-12s\n", "x", "Adelante", "Atras", "Centrada");
+  for(i=0; i<n; i++){
+    printf("%-10.2f %-12.4f %-12.4f %-12.4f\n", x[i], fpa[i], fpb[i], fpc[i]);
+  }
+
+  for(i=0; i<n; i++){
+    sda[i] = (fx(x[i+2]) - 2*fx(x[i+1]) + fx(x[i])) / (h*h);
+    sdb[i] = (fx(x[i-2]) - 2*fx(x[i-1]) + fx(x[i])) / (h*h);
+    sdc[i] = (fx(x[i+1]) - 2*fx(x[i]) + fx(x[i-1])) / (h*h);
+  }
+
+  printf("\nSegunda derivada");
+  printf("\n%-10s %-12s %-12s %-12s\n", "x", "Adelante", "Atras", "Centrada");
+  for(i=0; i<n; i++){
+    printf("%-10.2f %-12.4f %-12.4f %-12.4f\n", x[i], sda[i], sdb[i], sdc[i]);
+  }
+
+  FILE *fp = fopen("resultados.csv", "w");
+  if(fp == NULL){
+    printf("\nError al crear el archivo.\n");
+    return 1;
+  }
+
+  fprintf(fp, "x,Primera_Adelante,Primera_Atras,Primera_Centrada,Segunda_Adelante,Segunda_Atras,Segunda_Centrada\n");
+  for(i=0; i<n; i++){
+    fprintf(fp, "%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n",
+      x[i], fpa[i], fpb[i], fpc[i], sda[i], sdb[i], sdc[i]);
+  }
+  fclose(fp);
+
+  printf("\nArchivo 'resultados.csv' creado correctamente.\n");
+
+  return 0;
+}
+
+float fx(float x){
+    return x*x*x - 2*x + 4;
+}
