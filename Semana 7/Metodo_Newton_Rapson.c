2c3f305822c1c80869f972a17ffe674614130c47
Iveth2109 <02iveth21martinez@gmail.com>
2025-09-18 21:57:01 -0600

    Add files via upload

---
+++ Semana 7/Metodo_Newton_Rapson.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x){
+  return (exp(-x))-x;
+}
+
+double df(double x){
+  return (-exp(-x))-1;
+}
+
+int main(void){
+  double x0=0.0, x1, Err;
+  const double Err_max=1e-8;
+  const int maxIter=100;
+  int iter=0;
+
+  printf("Iter  \t  x_n   \t   f(x_n)   \t   Error\n");
+
+  while (iter<maxIter){
+    x1=x0-(f(x0)/df(x0));
+    Err=fabs((x1-x0)/x1);
+
+    printf("%d  \t  %f  \t  %f  \t  %f\n", iter, x0, f(x0), Err);
+
+    if(Err<Err_max){
+      printf("\nConvergio en %d iteraciones\n", iter);
+      printf("Raíz aproximada: %f\n", x1);
+      return 0;
+    }
+    x0=x1;
+    iter++;
+  }
+  printf("\nNo convergio después de %d iteraciones", maxIter);
+  printf("\nÚltimo valor %f   Error: %f\n", x1, Err);
+
+  return 0;
+}
