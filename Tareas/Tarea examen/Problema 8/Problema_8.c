4b93b773646f0e5cb55cdd5e8b3bf8ba87490432
Iveth2109 <02iveth21martinez@gmail.com>
2025-09-29 18:41:10 -0600

    Add files via upload

---
+++ Tareas/Tarea examen/Problema 8/Problema_8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double c, double W, double Q, double k, double V) {
+  return W - Q*c - k*V*sqrt(c);
+}
+
+int main(){
+  double V = 1e6;
+  double Q = 1e5;
+  double W = 1e6;
+  double k = 0.25;
+
+  double c0 = 4.0;
+  double d = 0.5;
+  double c_next, Err;
+  int nIter = 3;
+  int iter;
+
+  printf("Iter\t   c_i\t\t   c_i+1\t\t Err_rel (%%)\n");
+
+  for(iter = 1; iter <= nIter; iter++){
+    c_next = c0 - f(c0, W, Q, k, V) * d / (f(c0 + d, W, Q, k, V) - f(c0, W, Q, k, V));
+    Err = fabs((c_next - c0)/c_next) * 100.0;
+    printf("%d\t%lf\t%lf\t%lf\n", iter, c0, c_next, Err);
+    c0 = c_next;
+    }
+
+  printf("\nConcentración aproximada después de %d iteraciones: %lf g/m3\n", nIter, c_next);
+  printf("Error relativo porcentual en la última iteración: %lf %%\n", Err);
+
+  return 0;
+}
